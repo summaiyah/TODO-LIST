64b09708cf5f1d659731b348724d674e421b7766
summaiyah <summayakhalid731@gmail.com>
2024-11-18 12:24:56 +0500

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,92 @@
-import Image from "next/image";
+"use client"
+import { ModeToggle } from "@/components/ModeToggle";
+import { useState } from "react"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default function Home (){
+// define state 
+const[todos, setTodos] = useState([
+  {task:"laundry", id:1},
+])
+  const[inputVal, setInput] = useState("");
+  const[id, setId] = useState(0);
+  //functions
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const addItem=()=>{
+    let obj:any= todos.find(item => item.id == id)
+    if(obj){
+      let newArray = todos.filter(item=> item.id !== obj.id)
+      setTodos([...newArray,{task:inputVal, id:id}])
+    setInput("")
+    setId(0)
+    return
+    }
+    setTodos([...todos,{task:inputVal, id:id}])
+    setInput("")
+    setId(0)
+    
+  }
+
+  const editItem=(id:any)=>{
+    let obj:any= todos.find(item => item.id == id)
+    setInput(obj.task)
+    setId(obj.id)
+  }
+
+  const deleteItem=(id:any)=>{
+    let newArray = todos.filter(item=> item.id !== id)
+      setTodos([...newArray])
+  }
+  
+  return(
+    <div><ModeToggle/>
+    <div className="max-w-4xl  mx-auto p-5">
+      <h1 className="text-center text-[45px] font-mono"> TO-DO LIST</h1>
+
+      {/* start input div  */}
+      <div className="flex justify-between gap-10">
+        <input 
+        type="text"
+       value= {inputVal}
+        onChange={(e)=> setInput(e.target.value)}
+        className="w-[80%] p-3 rounded-l-md  rounded-r-md border-b  border-r focus:outline-none"
+        placeholder="Write Task"
+          />
+          <input
+          type="number"
+          value={id}
+          onChange={(e:any)=>setId(e.target.value)}
+           className="w-[15%] p-3 rounded-l-md  rounded-r-md border-b  border-r focus:outline-none" placeholder=" Type ID"/>
+        { <button 
+        onClick={addItem}
+        className=" w-[05%] bg-black text-white text-[20px] p-2 border-none rounded-md hover:bg-gray-700  dark:bg-white dark:text-black">+</button>}
+      </div>
+      {/* end input div  */}
+      <div className="grid grid-cols-1 mt-5  ">
+        {/* grid items */}
+        {
+          todos.map((item:any, i:any)=>{
+            return(
+              
+              <div className="shadow p-3 w-[90%] "key={i}>
+          <div className="flex justify-between">
+          <span className="shadow rounded-full h-5 w-5">{i+1}</span>
+          <span 
+          onClick={()=>deleteItem(item.id)}
+          className="shadow rounded-full h-5 w-5 cursor-pointer hover:bg-red-700 text-center hover:text-white">X</span>
+          </div>
+          {/* data div  */}
+        <div className="mt-5 text-[30px]">{item.task}</div>
+        <div>
+          <h2 onClick={()=>editItem(item.id)} className="text-right cursor-pointer hover:text-green-500">Edit</h2>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+            )
+          })
+        }
+        
+        </div>
+      </div>
     </div>
-  );
+    
+  )
 }
